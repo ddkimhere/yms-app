f9b1e4fa0c82cba74fe2e72c1e3d1863acc0765c
ddkimhere <ddkim1984@gmail.com>
2026-08-16 12:54:10 +0900

    Use Android API 36 for stable widget build

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.yms.mastertrack"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yms.mastertrack"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
