55e0ba2306905de6e4225c035bbfc145e3e625a3
ddkimhere <ddkim1984@gmail.com>
2026-08-20 17:13:00 +0900

    Remove unused admin multirole loader

--- js/admin-multirole-loader.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  if(!location.pathname.endsWith('/admin.html')) return;
-  const s=document.createElement('script');
-  s.src='js/admin-multirole-fix.js';
-  document.head.appendChild(s);
-})();
